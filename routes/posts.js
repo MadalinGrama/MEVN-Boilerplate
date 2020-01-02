08e83b9cfb53d17e8c78adb3b4b039851b272c39
MadalinGrama <grama.m.madalin@gmail.com>
2020-01-02 23:35:39 +0200

    Get posts

--- routes/posts.js
+++ routes/posts.js
@@ -2,3 +2,14 @@ const express = require('express');
 const mongodb = require('mongodb');
 
 const router = express.Router();
+
+// Get Posts
+
+router.get('/', (req, res) =>{
+  res.send('Hello World');
+});
+
+// Add Posts
+
+
+// Delete Posts
